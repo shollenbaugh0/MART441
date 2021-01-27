6440aa3768b821e32323e3ffe2318a346fcae019
shollenbaugh0 <70164033+shollenbaugh0@users.noreply.github.com>
2021-01-27 13:20:23 -0500

    updated console log again

--- HW-2/script.js
+++ HW-2/script.js
@@ -1,5 +1,8 @@
 // Example of an external JavaScipt File
 
-console.log("Hello world!");
+console.log("Hello world! Favorite websites include autostraddle, sites with
+free design resources and Youtube");
+console.log("Favorite games include Harvest Moon: Magical Melody, Animal Crossing,
+and Fortnite.")
 // If you open the web console,
 // you will see "Hello World!" printed, just like before.
