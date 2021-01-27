965241a404562f9fcd99f9bff42186806c79067f
shollenbaugh0 <70164033+shollenbaugh0@users.noreply.github.com>
2021-01-27 13:21:53 -0500

    testing script

--- HW-2/script.js
+++ HW-2/script.js
@@ -1,8 +1,6 @@
 // Example of an external JavaScipt File
 
 console.log("Hello world! Favorite websites include autostraddle, sites with
-free design resources and Youtube");
-console.log("Favorite games include Harvest Moon: Magical Melody, Animal Crossing,
-and Fortnite.")
+free design resources and streaming sites");
 // If you open the web console,
 // you will see "Hello World!" printed, just like before.
