27ff44b41a9d92c748cffdc2d310c1ce5cb6a3f1
shollenbaugh0 <70164033+shollenbaugh0@users.noreply.github.com>
2021-01-27 13:14:24 -0500

    updated console log

--- HW-2/script.js
+++ HW-2/script.js
@@ -1,5 +1,6 @@
 // Example of an external JavaScipt File
 
-console.log("Hello World!");
+console.log("3 favorite websites:
+1. autostraddle.com");
 // If you open the web console,
 // you will see "Hello World!" printed, just like before.
