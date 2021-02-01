cf7f0c7119421428a10b27d9e12d8d4cbe035ec8
shollenbaugh0 <70164033+shollenbaugh0@users.noreply.github.com>
2021-02-01 14:30:49 -0500

    added homework 3

---
+++ HW-3/main.js
@@ -0,0 +1,73 @@
+
+function storyFunction(choice) {
+    var answer1 = document.getElementById("choice1").innerHTML;
+    var answer2 = document.getElementById("choice2").innerHTML;
+    if (choice == 1 && answer1 == "Yes") {
+        document.getElementById("story").innerHTML = "Sappho enters her home to see her family. She could tell them the truth or avoid it entirely. What should she do?";
+        document.getElementById("choice1").innerHTML = "Talk to family";
+        document.getElementById("choice2").innerHTML = "Go to room";
+    } else if (choice == 2 && answer2 == "No") {
+        document.getElementById("story").innerHTML = "Sappho begins to leave but something encourages her to be brave.";
+        document.getElementById("choice1").innerHTML = "Begin a conversation";
+        document.getElementById("choice2").innerHTML = "Watch quietly";
+    } else if (choice == 1 && answer1 == "Begin conversation") {
+        document.getElementById("story").innerHTML = "Sappho engages in a conversation with her family. What should she do next?";
+        document.getElementById("choice1").innerHTML = "Listen quietly";
+        document.getElementById("choice2").innerHTML = "Attempt to make her announcement";
+    } else if (choice == 2 && answer2 == "Listen quietly") {
+        document.getElementById("story").innerHTML = "Sappho is still in the kitchen with her family. What should she do next?";
+        document.getElementById("choice1").innerHTML = "Tell them";
+        document.getElementById("choice2").innerHTML = "Get a drink";
+    } else if (choice == 1 && answer1 == "Go ahead and be brave") {
+        document.getElementById("story").innerHTML = "Sappho decides to tell them even if she's unsure of how they will react.  Will it work out?'";
+        document.getElementById("choice1").innerHTML = "Yes it will work";
+        document.getElementById("choice2").innerHTML = "No it will not work";
+    } else if (choice == 2 && answer2 == "Scan the situation") {
+        document.getElementById("story").innerHTML = "Sappho decides to just look around for the moment. What should Sappho do now?";
+        document.getElementById("choice1").innerHTML = "Watch from afar";
+        document.getElementById("choice2").innerHTML = "Get in closer";
+    }
+    // need to do these
+    else if (choice == 1 && answer1 == "Evaluate their emotions") {
+        document.getElementById("story").innerHTML = "Sappho decides that evaluation is the best option." + "<br>Restart?";
+        document.getElementById("choice1").innerHTML = "Yes restart";
+        document.getElementById("choice2").innerHTML = "No quit";
+    } else if (choice == 2 && answer2 == "Stay clear") {
+        document.getElementById("story").innerHTML = "Sappho decides staying away is the best option" + "<br>Restart?";
+        document.getElementById("choice1").innerHTML = "Yes restart";
+        document.getElementById("choice2").innerHTML = "No quit";
+    } else if (choice == 1 && answer1 == "Leave") {
+        document.getElementById("story").innerHTML = "Pugster decides that swimming sounds most fun" + "<br>Restart?";
+        document.getElementById("choice1").innerHTML = "Yes restart";
+        document.getElementById("choice2").innerHTML = "No quit";
+    } else if (choice == 2 && answer2 == "Get a drink") {
+        document.getElementById("story").innerHTML = "Sappho decides it's time to drink" + "<br>Restart?";
+        document.getElementById("choice1").innerHTML = "Yes restart";
+        document.getElementById("choice2").innerHTML = "No quit";
+    } else if (choice == 1 && answer1 == "Yes it will work") {
+        document.getElementById("story").innerHTML = "Pugster thinks, yeah this will work out." + "<br>Restart?";
+        document.getElementById("choice1").innerHTML = "Yes restart";
+        document.getElementById("choice2").innerHTML = "No quit";
+    } else if (choice == 2 && answer2 == "No it will not work") {
+        document.getElementById("story").innerHTML = "Sappho thinks, I don't think this will work out." + "<br>Restart?";
+        document.getElementById("choice1").innerHTML = "Yes restart";
+        document.getElementById("choice2").innerHTML = "No quit";
+    } else if (choice == 1 && answer1 == "Watch from afar") {
+        document.getElementById("story").innerHTML = "Sappho decides it's time to just watch from a distance for safety." + "<br>Restart?";
+        document.getElementById("choice1").innerHTML = "Yes restart";
+        document.getElementById("choice2").innerHTML = "No quit";
+    } else if (choice == 2 && answer2 == "Get in closer") {
+        document.getElementById("story").innerHTML = "Sappho decides to get just a little closer, but not engage." + "<br>Restart?";
+        document.getElementById("choice1").innerHTML = "Yes restart";
+        document.getElementById("choice2").innerHTML = "No quit";
+    } else if (choice == 1 && answer1 == "Yes restart") {
+        document.getElementById("story").innerHTML = "Our protagonist, Sappho, is finally comfortable in who they are and is ready to tell the world. Are you ready to help them come out?";
+        document.getElementById("choice1").innerHTML = "Yes";
+        document.getElementById("choice2").innerHTML = "No";
+    } else if (choice == 2 && answer2 == "No quit") {
+        document.getElementById("story").innerHTML = "Sappho thanks you!";
+
+    }
+
+
+}
---
+++ HW-3/readme.md
@@ -0,0 +1,2 @@
+## Savanah Hollenbaugh
+My biggest challenge is always myself. When starting homework for this class, I tend to immediately panic and think that I will never be able to finish it because it's too complicated. However, once I start working, I find that things are not always as hard as they seem, and that I don't have to make them more complicated than they need to be. 
