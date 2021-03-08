be56183da8e61f812b4b60c4c7aa0930a3264495
shollenbaugh0 <70164033+shollenbaugh0@users.noreply.github.com>
2021-03-08 12:49:07 -0500

    updated hw 8

--- HW-8/scripts/main.js
+++ HW-8/scripts/main.js
@@ -1,6 +1,6 @@
-var paintswatchSelector = "#lesbian paint swatch";
-var allpaintswatches = new Array();
-class PaintswatchInfo{
+var dogSelector = "#dog";
+var allDogs = new Array();
+class DogInfo{
     constructor(selector, imagePath)
     {
         this.selector = selector;
@@ -24,19 +24,19 @@ class PaintswatchInfo{
 function initializeArray()
 {
 
-    var lesbian paint swatch = new PaintswatchInfo("#lesbian paint swatch", "images/lesbian paint swatch.png");
-    allPaintswatches.push(lesbian paint swatch);
+    var dog = new DogInfo("#dog", "images/heart.png");
+    allDogs.push(dog);
 
 }
 $(document).ready(function(){
     initializeArray();
-    console.log(allPaintswatches.length);
-    console.log(allPaintswatches[0].toString());
-    console.log(allPaintswatches[0].theSelector);
-    console.log(allPaintswatches[0].theImagePath);
+    console.log(allDogs.length);
+    console.log(allDogs[0].toString());
+    console.log(allDogs[0].theSelector);
+    console.log(allDogs[0].theImagePath);
 
 
-    $(allPaintswatches[0].theSelector).attr("src", allPaintswatches[0].theImagePath);
+    $(allDogs[0].theSelector).attr("src", allDogs[0].theImagePath);
 
     $("button").click(function(){
 
@@ -45,7 +45,65 @@ $(document).ready(function(){
         $("#third").toggle();
            setInterval(moveSquare, 1000);
 
-        $(allPaintswatches[0].theSelector).fadeOut(3).fadeIn(1);
+        $(allDogs[0].theSelector).fadeOut().fadeIn();
+
+    });
+
+});
+
+function moveSquare()
+{
+    $("#square").animate({left:250}).animate({top:400}).animate({left:0}).animate({top:300});
+}
+
+var dogSelector = "#dog1";
+var allDogs = new Array();
+class DogInfo{
+    constructor(selector, imagePath)
+    {
+        this.selector = selector;
+        this.imagePath = imagePath;
+    }
+
+    get theSelector(){
+        return this.selector;
+    }
+
+    get theImagePath(){
+        return this.imagePath;
+    }
+
+    toString()
+    {
+        return this.selector + ":" + this.imagePath;
+    }
+}
+
+function initializeArray()
+{
+
+    var dog = new DogInfo("#dog1", "images/lesbian powerpuff hearts.png");
+    allDogs.push(dog);
+
+}
+$(document).ready(function(){
+    initializeArray();
+    console.log(allDogs.length);
+    console.log(allDogs[0].toString());
+    console.log(allDogs[0].theSelector);
+    console.log(allDogs[0].theImagePath);
+
+
+    $(allDogs[0].theSelector).attr("src", allDogs[0].theImagePath);
+
+    $("button").click(function(){
+
+        $(".stuff").fadeOut();
+
+        $("#third").toggle();
+           setInterval(moveSquare, 1000);
+
+        $(allDogs[0].theSelector).fadeOut().fadeIn();
 
     });
 
